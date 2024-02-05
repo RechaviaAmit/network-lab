e398a0e45affb107386de9f591f0d665d7ed4386
Amit Rechavia <amit.rechavia@intel.com>
2024-02-05 19:04:45 +0200

    don't close the socket

--- WebServer.java
+++ WebServer.java
@@ -33,21 +33,32 @@ public class WebServer {
         }
 
         private void handleClientConnection() throws IOException {
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+            BufferedReader in = null;
+            PrintWriter out = null;
+            try {
+                while (true) {
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-                StringBuilder requestBuilder = new StringBuilder();
-                String line;
-                while (!(line = in.readLine()).isEmpty()) {
-                    requestBuilder.append(line).append("\r\n");
+                    StringBuilder requestBuilder = new StringBuilder();
+                    String line;
+                    while (!(line = in.readLine()).isEmpty()) {
+                        requestBuilder.append(line).append("\r\n");
+                    }
+                    // they ask to print the request
+                    System.out.println(requestBuilder.toString());
+                    HTTPRequest request = new HTTPRequest(requestBuilder.toString());
+                    new HTTPResponseHandler(request, out).handle();
                 }
-                // they ask to print the request
-                System.out.println(requestBuilder.toString());
-                HTTPRequest request = new HTTPRequest(requestBuilder.toString());
-                new HTTPResponseHandler(request, out).handle();
-
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             } finally {
-                clientSocket.close();
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
             }
         }
     }
